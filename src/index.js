d8038478c516791f1fe84a2b2b175e5b2ab9bfa6
Shivam Patel <patelsd96@gmail.com>
2020-05-11 22:06:25 -0400

    modified the axios file for netflify

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import "index.scss";
 
 import Application from "components/Application";
 
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 // the main reactDOM renderer for our react app, we render application to the root
 ReactDOM.render(<Application />, document.getElementById("root"));
